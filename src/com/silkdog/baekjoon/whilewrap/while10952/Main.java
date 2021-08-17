cd0bce7b28bb9a727399658634a45c465f27b5f9
JEON EUNPYO <jep0829@echomarketing.co.kr>
2021-08-17 19:58:10 +0900

    Commit before off work

---
+++ src/com/silkdog/baekjoon/whilewrap/while10952/Main.java
@@ -0,0 +1,31 @@
+package com.silkdog.baekjoon.whilewrap.while10952;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a, b, c;
+        String line;
+        while ((line = br.readLine()) != null ) {
+            if ("".equals(line) || "0 0".equals(line)) {
+                bw.flush();
+                bw.close();
+                br.close();
+                System.exit(0);
+            }
+            a = Integer.parseInt(line.split(" ")[0]);
+            b = Integer.parseInt(line.split(" ")[1]);
+            c = a + b;
+            bw.write(Integer.toString((c)) + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
